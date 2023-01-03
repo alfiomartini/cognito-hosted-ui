9aa246d2887e9d95207f07a7667f00187c10da89
Alfio Martini <alfio.martini@gmail.com>
2023-01-03 05:13:45 -0300

    remove signOut (cors issues)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
 import { CognitoUI } from "./components/CognitoUI";
 import { Navbar } from "./components/Navbar";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
-const SIGN_OUT_URL = `https://bultin-login.auth.sa-east-1.amazoncognito.com/logout?client_id=1l1pg2a87u7agm14jh6loovrbh&logout_uri=http://localhost:3005`;
+// const SIGN_OUT_URL = `https://bultin-login.auth.sa-east-1.amazoncognito.com/logout?client_id=1l1pg2a87u7agm14jh6loovrbh&logout_uri=http://localhost:3005`;
 
 function App() {
-  const doSignOut = async () => {
-    const resp = await fetch(SIGN_OUT_URL, { mode: "no-cors" });
-    console.log("resp", resp);
-  };
+  // const doSignOut = async () => {
+  //   const resp = await fetch(SIGN_OUT_URL, { mode: "no-cors" });
+  //   console.log("resp", resp);
+  // };
 
   return (
     <div>
-      <Navbar user={{}} doSignOut={doSignOut} />
+      <Navbar user={{}} />
       <div className="container">
         <Switch>
           <Route exact path="/">
@@ -21,9 +21,6 @@ function App() {
           </Route>
           <Route path="/signIn">
             <CognitoUI />
-          </Route>
-          <Route path="/signOut">
-            <Redirect to="/" />
           </Route>
         </Switch>
       </div>
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./styles.css";
 
-export const Navbar = ({ user, doSignOut }) => {
+export const Navbar = ({ user }) => {
   return (
     <div className="navbar">
       <div className="logo">Cognito Hosted-UI</div>
@@ -12,12 +12,6 @@ export const Navbar = ({ user, doSignOut }) => {
           Sign In
         </Link>
       )}
-      {user && (
-        <Link to="/signOut" className="link" onClick={() => doSignOut()}>
-          Sign Out
-        </Link>
-      )}
-      {/* {user && <div className="auth-user"> {user.attributes.email}</div>} */}
     </div>
   );
 };
