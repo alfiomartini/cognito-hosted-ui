a0c595ed69c5fae630a1429e6896434c3796b4a6
Alfio Martini <alfio.martini@gmail.com>
2023-01-05 22:15:58 -0300

    update port number

--- src/components/CognitoUI/index.jsx
+++ src/components/CognitoUI/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 
 export const CognitoUI = () => {
-  const HOSTED_UI_URL = `https://bultin-login.auth.sa-east-1.amazoncognito.com/login?client_id=1l1pg2a87u7agm14jh6loovrbh&response_type=code&scope=phone+email+openid+aws.cognito.signin.user.admin+profile&redirect_uri=http://localhost:3005`;
+  const HOSTED_UI_URL = `https://bultin-login.auth.sa-east-1.amazoncognito.com/login?client_id=1l1pg2a87u7agm14jh6loovrbh&response_type=code&scope=phone+email+openid+aws.cognito.signin.user.admin+profile&redirect_uri=http://localhost:3000`;
   useEffect(() => {
     window.location.href = HOSTED_UI_URL;
   }, [HOSTED_UI_URL]);
