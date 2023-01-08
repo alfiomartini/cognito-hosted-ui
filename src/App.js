005794bb12cdd411074c37895550a0b5b901bd93
Alfio Martini <alfio.martini@gmail.com>
2023-01-08 17:34:41 -0300

    update package.json

--- src/App.js
+++ src/App.js
@@ -23,11 +23,11 @@ function App() {
       <Navbar auth={user} doSignOut={doSignOut} />
       <div className="container">
         <Switch>
-          <Route exact path={["/", "/cognito-hosted-ui"]}>
+          <Route exact path={["/"]}>
             {!user && <h2>Sign in to start Cognito Hosted-UI</h2>}
             {user && <h2>Sign out restart with Cognito Hosted-UI</h2>}
           </Route>
-          <Route path={["/loading", "/cognito-hosted-ui/loading"]}>
+          <Route path={["/loading"]}>
             <Loading setUser={setUser} />
           </Route>
           <Route path="/signIn">
